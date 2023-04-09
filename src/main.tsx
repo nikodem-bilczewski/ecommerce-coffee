69fec2ba041cc7333ec94f4b261e247eac1cfb82
nikodem bilczewski <bilczewski.nikodem@gmail.com>
2023-04-09 13:46:50 +0200

    fix: change dir name routes to pages and move routes to main.tsx file

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
+import About from './pages/About'
+import Home from './pages/Home'
+import Products from './pages/Products'
+import Root from './pages/Root'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: 'products', element: <Products /> },
+      { path: 'about', element: <About /> },
+    ],
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
