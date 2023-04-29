0367655d84d255c67da5f9591666f8e7ef86ca51
nikodem bilczewski <bilczewski.nikodem@gmail.com>
2023-04-29 22:49:37 +0200

    refactor: extract logic from Events to useEvents hook

--- src/components/pages/Events/index.tsx
+++ src/components/pages/Events/index.tsx
@@ -1,26 +1,8 @@
-import { useState } from 'react'
+import useEvents from './useEvents'
 import Event from 'components/organisms/Event/index'
-import cuppingImg from 'assets/images/cupping.jpg'
-import baristaImg from 'assets/images/barista.jpg'
-import { CoffeeEvent } from 'components/organisms/Event/CoffeeEvent.type'
 
 const Events = () => {
-  const [coffeeEvents, setCoffeeEvents] = useState<CoffeeEvent[]>([
-    {
-      heading: 'Cupping',
-      description:
-        'Our cupping event is designed for coffee enthusiasts who want to learn about coffee tasting and flavor profiles. Come taste a variety of single-origin coffees, learn how to identify the unique characteristics of each bean, and discover the secrets of the perfect roast.',
-      image: cuppingImg,
-      date: '2023-05-30',
-    },
-    {
-      heading: 'Barista course',
-      description:
-        'Sign up for our barista course! Our expert instructors will teach you everything you need to know about espresso extraction, milk texturing, latte art, and more. Whether you are a coffee lover looking to improve your home brewing skills or an aspiring barista hoping to kickstart your career, our course is perfect for you.',
-      image: baristaImg,
-      date: '2023-06-27',
-    },
-  ])
+  const coffeeEvents = useEvents()
   return (
     <div className='m-16 mx-auto grid grid-cols-1 place-content-center gap-14 lg:grid-cols-2'>
       {coffeeEvents &&
