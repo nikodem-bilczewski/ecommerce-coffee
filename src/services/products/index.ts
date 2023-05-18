1cb7775a2422cfe4c438ce1a0c53e498710591c3
nikodem bilczewski <bilczewski.nikodem@gmail.com>
2023-05-18 13:53:42 +0200

    fix: change getProducts function to implicitly return generic type

--- src/services/products/index.ts
+++ src/services/products/index.ts
@@ -1,11 +1,8 @@
 import { ProductResponse } from 'components/pages/Products/Products.types'
 import { productsAPI } from 'config/axios'
 
-export const getProducts = async (
-  page: number,
-  perPage = 6,
-): Promise<ProductResponse> => {
-  const response = await productsAPI.get(
+export const getProducts = async (page: number, perPage = 6) => {
+  const response = await productsAPI.get<ProductResponse>(
     `api/products?perPage=${perPage}&page=${page}`,
   )
   return response.data
