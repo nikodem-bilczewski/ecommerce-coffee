fd48975337f017f028091b4d4ef3b30c57e6ef45
nikodem bilczewski <bilczewski.nikodem@gmail.com>
2023-06-18 21:09:50 +0200

    fix: change authAPI url and AuthResponse type

--- src/services/auth/index.ts
+++ src/services/auth/index.ts
@@ -3,11 +3,11 @@ import { authAPI } from 'config/axios'
 import { AuthResponse } from 'config/axios/axios.types'
 
 export const signUpFn = async (userData: FormInput): Promise<AuthResponse> => {
-  const response = await authAPI.post('/register', userData)
+  const response = await authAPI.post('/local/register', userData)
   return response.data
 }
 
 export const logInFn = async (userData: FormInput): Promise<AuthResponse> => {
-  const response = await authAPI.post('/login', userData)
+  const response = await authAPI.post('/local', userData)
   return response.data
 }
