9dae3f2053048ec8462256ef286cded800ab5e1a
nikodem bilczewski <bilczewski.nikodem@gmail.com>
2023-06-18 21:15:09 +0200

    feat: add UserProfile page

--- src/components/molecules/NavigationIcons/index.tsx
+++ src/components/molecules/NavigationIcons/index.tsx
@@ -1,32 +1,25 @@
 import ShoppingCartIcon from '@heroicons/react/24/outline/ShoppingCartIcon'
 import UserIcon from '@heroicons/react/24/outline/UserIcon'
-import { useContext } from 'react'
 import { Link } from 'react-router-dom'
 
 import IconWrapper from 'components/atoms/IconWrapper'
-import Button from 'components/molecules/Button'
-import { AuthContext } from 'contexts/AuthContext'
+import useAuthContext from 'contexts/AuthContext/useAuthContext'
 import useCartContext from 'contexts/CartContext/useCartContext'
 
 const NavigationIcons = () => {
-  const { user, logOut } = useContext(AuthContext)
+  const { user } = useAuthContext()
   const { toggleCart } = useCartContext()
+  const path = user ? 'user' : 'login'
   return (
     <div className='flex items-center justify-center gap-5'>
       <IconWrapper clickHandler={toggleCart}>
         <ShoppingCartIcon className='h-8 w-8 lg:h-10 lg:w-10' />
       </IconWrapper>
-      {user ? (
-        <Button secondary onClick={logOut}>
-          Logout
-        </Button>
-      ) : (
-        <IconWrapper>
-          <Link to='login'>
-            <UserIcon className='h-8 w-8 lg:h-10 lg:w-10' />
-          </Link>
-        </IconWrapper>
-      )}
+      <IconWrapper>
+        <Link to={path}>
+          <UserIcon className='h-8 w-8 lg:h-10 lg:w-10' />
+        </Link>
+      </IconWrapper>
     </div>
   )
 }
---
+++ src/components/pages/UserProfile/index.tsx
@@ -0,0 +1,38 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import { Link } from 'react-router-dom'
+
+import Button from 'components/molecules/Button'
+import useAuthContext from 'contexts/AuthContext/useAuthContext'
+
+const UserProfile = () => {
+  const { logOut } = useAuthContext()
+  const user = JSON.parse(localStorage.getItem('user')!)
+  const { username } = user.user
+  return (
+    <div className='flex flex-col justify-between  h-screen w-full mx-auto px-6 lg:px-8'>
+      <div className='flex w-full md:w-4/5 mt-16 flex-col mx-auto'>
+        <div className='flex justify-between'>
+          <h1 className='self-start text-4xl font-normal'>Profile</h1>
+          <div className='w-40'>
+            <Button secondary onClick={logOut}>
+              Logout
+            </Button>
+          </div>
+        </div>
+        <span className='mb-5 h-1 w-full border-b border-gray-400'></span>
+        <div className='text-2xl w-full mx-auto'>
+          <h2>Hello {username}</h2>
+        </div>
+      </div>
+      <div>
+        <div className='w-60 lg:w-80 mx-auto mb-40'>
+          <Link to='/products'>
+            <Button secondary>Check our shop</Button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default UserProfile
